f3180623815b8c1673f666ecbb28da879b5b6b23
coluc <coluc@LAPTOP-TC6V3OK6>
2024-12-02 15:07:21 +0100

    primo commit

---
+++ array20/src/mainpackage/Arrayn20.java
@@ -0,0 +1,74 @@
+/*Write a Java program to check if the value 20 appears three times and no 20's are next to each other in the array of integers.*/
+package mainpackage;
+
+public class Arrayn20 {
+
+	public static void main(String[] args) {
+		
+		
+		
+int array[]= {1,2,3,5,20,8,2,9,20,50,10,20,20,20};
+int i;
+int c=0;
+    for(i=0;i<array.length;i++){
+         //   System.out.println(array[i]);
+       if(array[i]==20){
+          c++;
+         }
+}
+System.out.println("Il numero 20 appare:"+c+"volte");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*int array[]= {1,2,3,5,20,8,2,9,20,50,10,20};
+		int i;
+		int c=0;
+		for(i=0;i<array.length;i++) {
+		
+			if(array[i]==20) {
+				if(array[i+1]==20 || array.length-1) {
+					System.out.println("il numero è vicino");
+				}
+				 c++;
+				 
+				 
+				 
+			}
+		   
+		}
+			
+		if(c==3) {
+			System.out.println("Il numero 20 è presente "+c+" o più volte");
+		}
+		if(c<3) {
+			System.out.println("Il numero non è presente 3 volte");
+		}*/
+			
+			
+		}
+}
+	
+
+
+
+
